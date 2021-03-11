cd2fce8f0834e02f41e96522bab75edca2f8b404
Thiago Leobons <4767061+TLeobons@users.noreply.github.com>
2021-03-11 13:45:43 +0100

    updater

--- src/App.jsx
+++ src/App.jsx
@@ -19,24 +19,40 @@ const App = () => {
     <Container className="App">
       <GlobalStyle />
       <NavMenu />
-      <Switch>
-        {menuItems.map((item, i) => (
-          <Route
-            key={i}
-            path={`/${item.path}`}
-            component={mapping[item.path]}
-          />
-        ))}
-        <Route path="/404" component={NotFound} />
-        <Route path="/" exact component={App} />
-        <Redirect to="/404" component={NotFound} />
-      </Switch>
+      <Main>
+        <Switch>
+          {menuItems.map((item, i) => (
+            <Route
+              key={i}
+              path={`/${item.path}`}
+              component={mapping[item.path]}
+            />
+          ))}
+          <Route path="/404" component={NotFound} />
+          <Route path="/" exact component={App} />
+          <Redirect to="/404" component={NotFound} />
+        </Switch>
+      </Main>
+      <SideBar>
+
+      </SideBar>
     </Container>
   );
 }
 
 export default App
 
-const Container = styled.main`
+const Container = styled.div`
   display: flex;
+  grid-template-columns: 240px 1fr 320px;
+`
+const Main = styled.main`
+  padding: 40px;
+  width: 100%;
+
+`
+const SideBar = styled.aside`
+  height: 100vh;
+  border-left: 1px solid #F1F1F2;
+  padding: 40px;
 `
